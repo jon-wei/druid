81db8fc08d68223d534a0af711d6bf8c06bf2839
jon-wei <jon.wei@imply.io>
2018-07-31 16:36:05 -0700

    PR 5734

--- indexing-hadoop/src/main/java/io/druid/indexer/partitions/MultipleDimensionPartitionsSpec.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/partitions/MultipleDimensionPartitionsSpec.java
@@ -1,18 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. Metamarkets licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -22,26 +22,19 @@ package io.druid.indexer.partitions;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import io.druid.indexer.DeterminePartitionsJob;
-import io.druid.indexer.HadoopDruidIndexerConfig;
-import io.druid.indexer.Jobby;
-
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.List;
 
-public class SingleDimensionPartitionsSpec extends DimensionPartitionsSpec
+public class MultipleDimensionPartitionsSpec extends DimensionPartitionsSpec
 {
   @JsonCreator
-  public SingleDimensionPartitionsSpec(
-      @JsonProperty("partitionDimension") @Nullable String partitionDimension,
+  public MultipleDimensionPartitionsSpec(
+      @JsonProperty("partitionDimension") @Nullable List<String> partitionDimensions,
       @JsonProperty("targetPartitionSize") @Nullable Long targetPartitionSize,
       @JsonProperty("maxPartitionSize") @Nullable Long maxPartitionSize,
       @JsonProperty("assumeGrouped") @Nullable Boolean assumeGrouped
   )
   {
-    super(Lists.newArrayList(partitionDimension), targetPartitionSize, maxPartitionSize, assumeGrouped);
+    super(partitionDimensions, targetPartitionSize, maxPartitionSize, assumeGrouped);
   }
 }
