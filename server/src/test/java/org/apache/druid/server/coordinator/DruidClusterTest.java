2978a5d79510097a70d6b073b7da4abf3db84bfe
jon-wei <jon.wei@imply.io>
2020-05-28 02:29:37 -0700

    WIP

--- server/src/test/java/org/apache/druid/server/coordinator/DruidClusterTest.java
+++ server/src/test/java/org/apache/druid/server/coordinator/DruidClusterTest.java
@@ -37,7 +37,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableSet;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DruidClusterTest
